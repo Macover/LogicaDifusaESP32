66450ed44365a6f5ab631d53f87b69d0865b5d72
Macover <macover1507@hotmail.com>
2022-02-23 19:27:43 -0600

    first commit rama isai

---
+++ LogicaDifusaGra/FAMDistancia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDifusaGra
+{
+    internal class FAMDistancia
+    {
+        public string actuador1 { get; set; }
+        public string actuador2 { get; set; }
+    }
+}
